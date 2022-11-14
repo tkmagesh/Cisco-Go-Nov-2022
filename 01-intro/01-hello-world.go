44047630052fcdd4d30425fc3223360282f352b8
Magesh Kuppan <tkmagesh77@gmail.com>
2022-11-14 10:36:12 +0530

    Hello world in go

---
+++ 01-intro/01-hello-world.go
@@ -0,0 +1,16 @@
+/* package declaration (mandatory) */
+package main
+
+/* import dependency packages */
+import "fmt"
+
+/* package scope type / variable declarations */
+
+/* package init function */
+
+/* main function */
+func main() {
+	fmt.Println("Hello World!")
+}
+
+/* other functions */
