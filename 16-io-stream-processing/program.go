9fcb25e4a9d060957784f3b5b1d21002e546c5e1
Magesh Kuppan <tkmagesh77@gmail.com>
2022-11-16 16:05:33 +0530

    reading data from file

---
+++ 16-io-stream-processing/program.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("data1.dat")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		data := scanner.Text()
+		fmt.Println(data)
+	}
+	fmt.Println("Done")
+}
